dfa67fc98267ee21d912a4d88173bad3f90311da
Denis <oweazzy@gmail.com>
2023-06-30 01:17:27 +0300

    server & options fix

--- src/pages/change-personal-data/index.ts
+++ src/pages/change-personal-data/index.ts
@@ -4,6 +4,7 @@ import Button from "../../partials/button";
 import Img from "../../partials/img";
 import img from "../../../static/img/cat_err.png";
 import Input from "../../partials/input";
+import {validate} from "../../utils/validation";
 
 export class ChangeData extends Block {
     constructor() {
@@ -22,35 +23,95 @@ export class ChangeData extends Block {
             type: "email",
             name: "email",
             placeholder: "email",
-            add_class: "page__input-small"
+            add_class: "page__input-small",
+            events: {
+                blur: (e) => {
+                    const emailValue = e.target.value.trim();
+                    const emailValidate = validate(emailValue,
+                        ['emailForm'])
+                    if (!emailValidate.isValid) {
+                        console.log(emailValidate.message)
+                    } else {
+                        console.log(`Email - ${e.target.value}`)
+                    }
+                }
+            }
         });
 
         this.children.loginInput = new Input({
             type: "text",
             name: "login",
             placeholder: "логин",
-            add_class: "page__input-small"
+            add_class: "page__input-small",
+            events: {
+                blur: (e) => {
+                    const loginValue = e.target.value.trim();
+                    const loginValidate = validate(loginValue,
+                        ['loginForm'])
+                    if (!loginValidate.isValid) {
+                        console.log(loginValidate.message)
+                    } else {
+                        console.log(`Логин - ${e.target.value}`)
+                    }
+                }
+            }
         });
 
         this.children.firstName = new Input({
             type: "text",
             name: "first_name",
             placeholder: "имя",
-            add_class: "page__input-small"
+            add_class: "page__input-small",
+            events: {
+                blur: (e) => {
+                    const firstName = e.target.value.trim();
+                    const firstNameValid = validate(firstName,
+                        ['nameForm'])
+                    if (!firstNameValid.isValid) {
+                        console.log(firstNameValid.message)
+                    } else {
+                        console.log(`Имя - ${e.target.value}`)
+                    }
+                }
+            }
         });
 
         this.children.secondName = new Input({
             type: "text",
             name: "first_name",
             placeholder: "фамилия",
-            add_class: "page__input-small"
+            add_class: "page__input-small",
+            events: {
+                blur: (e) => {
+                    const secondName = e.target.value.trim();
+                    const secondNameValid = validate(secondName,
+                        ['nameForm'])
+                    if (!secondNameValid.isValid) {
+                        console.log(secondNameValid.message)
+                    } else {
+                        console.log(`Фамилия - ${e.target.value}`)
+                    }
+                }
+            }
         });
 
         this.children.phoneInput = new Input({
             type: "text",
             name: "phone",
             placeholder: "телефон",
-            add_class: "page__input-small"
+            add_class: "page__input-small",
+            events: {
+                blur: (e) => {
+                    const phone = e.target.value.trim();
+                    const phoneValid = validate(phone,
+                        ['phoneForm'])
+                    if (!phoneValid.isValid) {
+                        console.log(phoneValid.message)
+                    } else {
+                        console.log(`Телефон - ${e.target.value}`)
+                    }
+                }
+            }
         });
 
         this.children.saveBtn = new Button({
@@ -59,12 +120,20 @@ export class ChangeData extends Block {
             type: "submit",
             events: {
                 click: (e) => {
-                    e.preventDefault()
+                    e.preventDefault();
+                    const formData = {
+                        email: this.children.mailInput.element.value.trim(),
+                        login: this.children.loginInput.element.value.trim(),
+                        firstName: this.children.firstName.element.value.trim(),
+                        secondName: this.children.secondName.element.value.trim(),
+                        phone: this.children.phoneInput.element.value.trim(),
+                    };
+                    console.log(formData);
                 }
             }
-        })
+        });
 
-        this.children.bakcBtn = new Button({
+        this.children.backBtn = new Button({
             label: "назад",
             add_class: "btn-medium",
             type: "submit",
